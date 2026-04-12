95c98886ad1f39a662ad85b28c86b5363e52bdd8
Rerurate514 <rerurateyuto@gmail.com>
2026-04-12 10:15:53 +0900

    deploycommandにconsoleを追加

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,14 +1,19 @@
 import { REST, Routes } from 'discord.js';
 import * as pingCommand from './commands/botest/ping.js';
+import * as onBoardingRoleCommand from './commands/onboarding/add-yearify-role.js';
 
 //npx tsx --env-file=.env src/deploy-commands.tsでデプロイ
 
-const commands = [pingCommand.data.toJSON()];
+const commands = [
+    pingCommand.data.toJSON(),
+    onBoardingRoleCommand.data.toJSON()
+];
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN!);
 
 (async () => {
     try {
         console.log('コマンド登録開始...');
+        console.log(commands);
         await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID!,
