f5ed0b6928a9702db50552dd2c6a973335dde5a7
Rerurate514 <rerurateyuto@gmail.com>
2026-04-12 09:20:37 +0900

    デプロイスクリプトを追加

---
+++ src/deploy-commands.ts
@@ -0,0 +1,23 @@
+import { REST, Routes } from 'discord.js';
+import * as pingCommand from './commands/botest/ping.js';
+
+//npx tsx --env-file=.env src/deploy-commands.tsでデプロイ
+
+const commands = [pingCommand.data.toJSON()];
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN!);
+
+(async () => {
+    try {
+        console.log('コマンド登録開始...');
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID!,
+                process.env.GUILD_ID!
+            ),
+            { body: commands }
+        );
+        console.log('コマンド登録完了！');
+    } catch (error) {
+        console.error(error);
+    }
+})();
