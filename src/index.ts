5ba68fcaf8a0d2c05afeb8b8c92cdf991644cf78
Rerurate514 <rerurateyuto@gmail.com>
2026-04-13 20:31:46 +0900

    インタラクションをtextとbuttonで分離

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import { client } from "./setup/client.js";
 import "./setup/ready.js";
-import "./setup/interaction.js";
+import "./setup/interaction-text.js";
+import "./setup/interaction-button.js";
 
 await client.login(process.env.DISCORD_TOKEN);
--- src/setup/interaction-button.ts
+++ src/setup/interaction-button.ts
@@ -2,26 +2,8 @@ import { Events } from 'discord.js';
 import * as onBoardingRoleCommand from '../commands/onboarding/add-yearify-role.js';
 import * as onBoardingInterestCommand from '../commands/onboarding/add-interest-role.js';
 import { client } from './client.js';
-import { commands } from './commands.js';
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.isChatInputCommand()) {
-        const command = commands.get(interaction.commandName);
-        if (!command) return;
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'エラーが発生しました', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'エラーが発生しました', ephemeral: true });
-            }
-        }
-    }
-
     const allRoleButtons = [
         ...onBoardingRoleCommand.roleButtons, 
         ...onBoardingInterestCommand.interestButtons
