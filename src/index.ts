54167ddf2a912634ffb9e6428b7939b8cd782cc1
Rerurate514 <rerurateyuto@gmail.com>
2026-04-13 20:39:29 +0900

    loginをindexから分離

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
-import { client } from "./setup/client.js";
 import "./setup/ready.js";
 import "./setup/interaction-text.js";
 import "./setup/interaction-button.js";
+import "./setup/login.js";
 
-await client.login(process.env.DISCORD_TOKEN);
---
+++ src/setup/login.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved */
+import { client } from "./client.js";
+
+await client.login(process.env.DISCORD_TOKEN);
